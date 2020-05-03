24422f64a324c45db6f8bb160c17787858249e8a
Arthur Rio <arthur@minervaproject.com>
2020-05-03 22:21:55 +0200

    Start trying things

---
+++ assembly_language_tutorial/asmtut.s
@@ -0,0 +1,17 @@
+.global_start
+
+_start:
+  MOV R0, #0 @ 0000
+  MOV R1, #1 @ 0001
+  B _continue_loop
+
+_loop:
+  ADD R0, R0, R1
+
+_continue_loop:
+  CMP R0, #9
+  BLE _loop
+
+end:
+  MOV R7, #1
+  SWI 0
